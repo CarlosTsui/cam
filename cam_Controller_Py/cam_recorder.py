9eac803666ea92df54929c380284319deb8f11ef
pentium3 <celerond@msn.com>
2016-04-04 20:32:39 +0800

    adding engine controlling module

--- cam_Controller_Py/cam_recorder.py
+++ cam_Controller_Py/cam_recorder.py
@@ -8,6 +8,9 @@ from pyaudio import PyAudio, paInt16
 from datetime import datetime 
 import wave
 import multiprocessing as mulp
+import serial
+import time
+import math
 
 def rec_audio(stat,filename,queue):
 	NUM_SAMPLES = 200
@@ -46,61 +49,75 @@ def rec_audio(stat,filename,queue):
 	queue.put("wav_sav_ok")
 
 def rec_video(stat,framecnt,filename,queue):
-    framelist=[]
-    cap=cv2.VideoCapture(1)
-    capsize = (int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)),int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)))
-    fourcc = cv2.cv.CV_FOURCC('M','S','V','C')
-    cv2.namedWindow("test")
-    framecnt.value=0
-    success, frame = cap.read()
-    cv2.imwrite("./temp_frame/0.jpg",frame)
-    framelist.append("./temp_frame/0.jpg")
-    framecnt.value+=1
-    queue.put("rotate_start")
-    queue.put("audio_start")
-    time_start=clock()
+	framelist=[]
+	cap=cv2.VideoCapture(1)
+	capsize = (int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)),int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)))
+	fourcc = cv2.cv.CV_FOURCC('M','S','V','C')
+	cv2.namedWindow("test")
+	framecnt.value=0
+	success, frame = cap.read()
+	cv2.imwrite("./temp_frame/0.jpg",frame)
+	framelist.append("./temp_frame/0.jpg")
+	framecnt.value+=1
+	queue.put("rotate_start")
+	queue.put("audio_start")
+	time_start=clock()
 
-    while True:
-        success, frame = cap.read()
-        cv2.imwrite("./temp_frame/"+str(framecnt.value)+".jpg",frame)
-        framelist.append("./temp_frame/"+str(framecnt.value)+".jpg")
-        framecnt.value+=1
-        cv2.imshow("test", frame)
-        
-        #print(frame.shape)
-        #print(type(frame))
+	while True:
+		success, frame = cap.read()
+		cv2.imwrite("./temp_frame/"+str(framecnt.value)+".jpg",frame)
+		framelist.append("./temp_frame/"+str(framecnt.value)+".jpg")
+		framecnt.value+=1
+		cv2.imshow("test", frame)
 
-        key=cv2.waitKey(10)
-        c = chr(key & 255)
-        if c in ['q', 'Q', chr(27)]:
-            time_finish=clock()
-            #with stat.get_lock()
-            stat.value = 1
-            break
+		#print(frame.shape)
+		#print(type(frame))
 
-    cap.release()
-    cv2.destroyAllWindows()
+		key=cv2.waitKey(10)
+		c = chr(key & 255)
+		if c in ['q', 'Q', chr(27)]:
+			time_finish=clock()
+			#with stat.get_lock()
+			stat.value = 1
+			break
 
-    while True:
-    	signal=queue.get()
-    	if(signal=="wav_sav_ok"):
-    		print("wav_completed")
-    		break
+	cap.release()
+	cv2.destroyAllWindows()
 
-   	print("video_start: "+str(time_start))
-    print("video_end: "+str(time_finish))
-    print("video_duration (sec): "+str(time_finish-time_start))   #duration (second)
-    print("total frames: "+str(framecnt.value))
-    cvfps=framecnt.value/(time_finish-time_start)
-    print("fps: "+str(cvfps))                               #fps
+	while True:
+		signal=queue.get()
+		if(signal=="wav_sav_ok"):
+			print("wav_completed")
+			break
 
-    clip=ImageSequenceClip(framelist,fps=cvfps)
-    audio_clip=AudioFileClip("./temp_frame/"+filename+".wav")
-    sndclip=clip.set_audio(audio_clip)
-    sndclip.to_videofile("./"+filename+".mp4",fps=cvfps)
+	print("video_start: "+str(time_start))
+	print("video_end: "+str(time_finish))
+	print("video_duration (sec): "+str(time_finish-time_start))   #duration (second)
+	print("total frames: "+str(framecnt.value))
+	cvfps=framecnt.value/(time_finish-time_start)
+	print("fps: "+str(cvfps))                               #fps
+
+	clip=ImageSequenceClip(framelist,fps=cvfps)
+	audio_clip=AudioFileClip("./temp_frame/"+filename+".wav")
+	sndclip=clip.set_audio(audio_clip)
+	sndclip.to_videofile("./"+filename+".mp4",fps=cvfps)
 
 def engine_rotating(stat,framecnt,queue):
-	classfier=cv2.CascadeClassifier("D:\\Anaconda\\haarcascade_frontalface_alt.xml")
+	pi=3.14159265358979723846
+	ags=[0 for x in range(0,20)]        #angle_sin
+	agc=[0 for x in range(0,20)]        #angle_cos
+	agh=[0 for x in range(0,20)]
+	angle=22.5
+	for i in range(1,9):
+		agh[i]=2*pi*angle/360
+		ags[i]=math.sin(agh[i])
+		agc[i]=math.cos(agh[i])
+		print(i,angle,agh[i],agc[i])
+		angle+=45;
+
+	ser=serial.Serial('COM4',9600)
+	ser.write("sssssssss")
+	classfier=cv2.CascadeClassifier("D:\\HFUTProject\\cam\\cam_Controller_Py\\haarcascade_frontalface_alt.xml")
 	color = (255,0,0)
 	while True:
 		signal=queue.get()
@@ -110,6 +127,7 @@ def engine_rotating(stat,framecnt,queue):
 	print("eng_start")
 
 	while True:
+		time.sleep(0.01)			##??
 		if(stat.value==1):
 			break
 		cnt=framecnt.value-1
@@ -122,12 +140,67 @@ def engine_rotating(stat,framecnt,queue):
 		h, w = size
 		minSize=(w/divisor, h/divisor)
 		faceRects = classfier.detectMultiScale(image, 1.2, 2, cv2.CASCADE_SCALE_IMAGE,minSize)
+		print(cnt,len(faceRects))
 		if len(faceRects)>0:
 			for faceRect in faceRects:
 				x, y, w, h = faceRect
 				print("detected face: ",x,y,w,h)
-				#cv2.rectangle(frame, (x, y), (x+w, y+h), color)
-				#TODO: Please Code Here
+				#cv2.rectangle(frame, (x, y), (x+w, y+h), color,3)
+				cx=x+(w/2)-320
+				cy=-(y+(h/2)-240)
+				cz=math.sqrt(cx*cx+cy*cy)
+				angle_cos=cy/cz
+				angle_sin=cx/cz
+				#640x480    center:320,240
+
+				if((abs(cx)>100)or(abs(cy)>75)):
+					print(x,x+w,y,y+h,"   ",cx,cy,angle_sin,angle_cos)
+					if(angle_sin>0):        #AQWED
+						if(angle_cos>agc[1]):           #D
+							ser.write("sw")
+						elif(angle_cos>agc[2]):      #E
+							ser.write("se")
+						elif(angle_cos>agc[3]):      #W
+							ser.write("sdd")
+						elif(angle_cos>agc[4]):      #Q
+							ser.write("sc")
+						else:                           #A
+							ser.write("sxx")
+					elif(angle_sin<=0):
+						if(angle_cos>agc[8]):           #D
+							ser.write("sw")
+						elif(angle_cos>agc[7]):      #C
+							ser.write("sq")
+						elif(angle_cos>agc[6]):      #X
+							ser.write("saa")
+						elif(angle_cos>agc[5]):      #Z
+							ser.write("sz")
+						else:                           #A
+							ser.write("sxx")
+					elif((abs(cx)>20)or(abs(cy)>15)):
+						print(x,x+w,y,y+h,"   ",cx,cy,angle_sin,angle_cos)
+						if(angle_sin>0):        #AQWED
+							if(angle_cos>agc[1]):           #D
+								ser.write("sw")
+							elif(angle_cos>agc[2]):      #E
+								ser.write("se")
+							elif(angle_cos>agc[3]):      #W
+								ser.write("sdd")
+							elif(angle_cos>agc[4]):      #Q
+								ser.write("sc")
+							else:                           #A
+								ser.write("sx")
+						elif(angle_sin<=0):
+							if(angle_cos>agc[8]):           #D
+								ser.write("sw")
+							elif(angle_cos>agc[7]):      #C
+								ser.write("sq")
+							elif(angle_cos>agc[6]):      #X
+								ser.write("saa")
+							elif(angle_cos>agc[5]):      #Z
+								ser.write("sz")
+							else:                           #A
+								ser.write("sx")
 
 	print("rotating finish")
 
---
+++ cam_GUI/cam/cam/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+
+namespace cam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ScriptRuntime pyRuntime = Python.CreateRuntime();
+            dynamic pyobj = pyRuntime.UseFile("hello.py");
+
+            Console.WriteLine(pyobj.welcome("1212"));
+         
+        }
+    }
+}
